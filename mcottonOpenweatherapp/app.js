a5e4c5ff32df821c34f139cb3d0815aa68dd9e4d
Micah-Cotton <cottonmicaht@gmail.com>
2022-02-04 02:09:02 +0000

    IT FINALLY WORKS!

--- mcottonOpenweatherapp/app.js
+++ mcottonOpenweatherapp/app.js
@@ -3,16 +3,20 @@ const app = {
 	document
 	   .getElementById('btnGet')
 	   .addEventListener('click', app.fetchWeather);
+
+//	document
+//	   .getElementById('btnCurrent')
+//	   .addEventListener('click', app.getLocation);
   },
 
   fetchWeather: (ev) => {
 	//use values from zip to fetch weather
-
-    let zip = document.getElementById('zip').value;
+    let lat = document.getElementById('latitude').value;
+    let lon = document.getElementById('longitude').value;
     let key = 'API-KEY-REDACTED';
     let lang = 'en';
     let units = 'imperial';
-    let url = `http://api.openweathermap.org/data/2.5/forecast?zip=${zip}&appid=${key}&lang=${lang}&units=${units}`;
+    let url = `http://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${key}&lang=${lang}&units=${units}`;
 	//fetch the weather
 
     fetch(url)
@@ -48,22 +52,22 @@ const app = {
 		     />
 		     <div class="card-body">
 		       <h3 class="card-title">{$day.weather[0].main}</h3>
-		       <p class="card-text">High ${day.temp.max}&deg;C Low ${
+		       <p class="card-text">High: ${day.temp.max}&deg;F Low ${
 		day.temp.min
-	      }&deg;C</p>
-			  <p class="card-text">High Feels like ${
+	      }&deg;F</p>
+			  <p class="card-text">High Feels like: ${
 			     day.feels_like.day
-			  }&deg;C</p>
-			  <p class="card-text">Pressure ${day.pressure}mb</p>
-			  <p class="card-text">Humanity ${day.huidity}%</p>
-			  <p class="card-text">UV Index ${day.uvi}</p>
-			  <p class="card-text">Precipitation ${day.pop * 100}%</p>
-			  <p class="card-text">Dewpoint ${day.dew_point}</p>
-			  <p class="card-text">Wind ${day.wind_speed}m/s, ${
+			  }&deg;F</p>
+			  <p class="card-text">Pressure: ${day.pressure}mb</p>
+			  <p class="card-text">Humidity: ${day.huidity}%</p>
+			  <p class="card-text">UV Index: ${day.uvi}</p>
+			  <p class="card-text">Precipitation: ${day.pop * 100}%</p>
+			  <p class="card-text">Dewpoint: ${day.dew_point}</p>
+			  <p class="card-text">Wind: ${day.wind_speed}MPH, ${
 		day.wind_deg
 	      }&deg;</p>
-			<p class="card-text">Sunrise ${sr}</p>
-			<p class="card-text">Sunset ${ss}</p>
+			<p class="card-text">Sunrise: ${sr}</p>
+			<p class="card-text">Sunset: ${ss}</p>
 		    </div>
 		  </div>
 		</div>
